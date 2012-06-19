10933a76171a0f19cf265d5a7ea763dde71428fe
Srimal Jayawardena <srimalj@gmail.com>
2012-06-19 17:48:52 +1000

    Script to generate some test data using vincents toolbox

---
+++ src/libmv_opencv/mvr/mvr_test/gendata.m
@@ -0,0 +1,41 @@
+% Create test data using:
+%
+% @misc{vincentsSFMToolbox,
+%  Author = {Vincent Rabaud},
+%  Title = {Vincent's {S}tructure from {M}otion {T}oolbox},
+%  howpublished = {\url{http://vision.ucsd.edu/~vrabaud/toolbox/}}
+% }
+% The previous home for that project was on http://vision.ucsd.edu/~vrabaud/toolbox/doc/
+
+system(' rm -rf S* K* W* P*');
+
+
+N=5;
+F=3;
+
+anim=generateToyAnimation( 0,'nPoint',N,'nFrame',F);
+% playAnim(anim);
+
+S=anim.S;
+csvwrite('S',S);
+
+Sh=[S;ones(1,N)];
+
+
+
+for i=1:F
+       
+    W=anim.W(:,:,i);
+    P=anim.P(:,:,i);
+    K=eye(3,3);
+    
+    % Validate
+    Wh=K*P*Sh;
+    assert(norm(Wh(1:2,:)-W) == 0);
+    
+    
+    csvwrite(['W' int2str(i)],W);
+    csvwrite(['P' int2str(i)],P);
+    csvwrite(['K' int2str(i)],K);
+    
+end
