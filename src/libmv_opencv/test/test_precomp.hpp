e2421d42eb5e63af510e89daf1fa47f9cf82cb50
Pablo Speciale <pablo.speciale@gmail.com>
2012-06-26 21:03:09 -0300

    Test for projection (empty for now)

---
+++ src/libmv_opencv/test/test_precomp.hpp
@@ -0,0 +1,11 @@
+#ifdef __GNUC__
+#  pragma GCC diagnostic ignored "-Wmissing-declarations"
+#endif
+
+#ifndef __OPENCV_TEST_PRECOMP_HPP__
+#define __OPENCV_TEST_PRECOMP_HPP__
+
+#include "opencv2/core/core.hpp"
+#include <iostream>
+
+#endif
