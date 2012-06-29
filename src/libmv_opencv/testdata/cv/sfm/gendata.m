c50935fba2f08f19da65068895692ae4d18e9399
Srimal Jayawardena <srimalj@gmail.com>
2012-06-29 16:57:20 +1000

    Read test data from location given by export OPENCV_TEST_DATA_PATH=/path/to/testdata (bash)

---
+++ src/libmv_opencv/testdata/cv/sfm/gendata.m
@@ -0,0 +1,15 @@
+% Create test data using:
+%
+% @misc{vincentsSFMToolbox,
+%  Author = {Vincent Rabaud},
+%  Title = {Vincent's {S}tructure from {M}otion {T}oolbox},
+%  howpublished = {\url{http://vision.ucsd.edu/~vrabaud/toolbox/}}
+% }
+% The previous home for that project was on http://vision.ucsd.edu/~vrabaud/toolbox/doc/
+% 
+
+% anim=generateToyAnimation( 0,'nPoint',5,'nFrame',3);
+% anim2cvyaml('rnd_N5_F3.yml',anim);
+
+anim=generateToyAnimation( 0,'nPoint',10,'nFrame',3);
+anim2cvyaml('rnd_N10_F3.yml',anim);
