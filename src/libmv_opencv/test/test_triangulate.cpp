2d7dde28c2106fb55511a074455964024d0aadce
Pablo Speciale <pablo.speciale@gmail.com>
2012-07-10 23:24:18 -0300

    * Added "Sfm_triangulate.TriangulateDLT" test * Fix code style

--- src/libmv_opencv/test/test_triangulate.cpp
+++ src/libmv_opencv/test/test_triangulate.cpp
@@ -35,38 +35,81 @@
 
 #include "test_precomp.hpp"
 
+#include "libmv/multiview/test_data_sets.h"
+#include "libmv/numeric/numeric.h"
+
+#include <opencv2/core/eigen.hpp>
+
 using namespace cv;
 using namespace std;
 using namespace cvtest;
 
 TEST(Sfm_triangulate, twoViewAffine)
 {
-  int nviews=2;
-  int npts=10;
-  vector<Point3d> points3d;
-  vector<Point3d> points3d_estimated;
-  vector<Mat> projection_matrices;
-  vector<vector<Point2d> > points2d;
+    int nviews=2;
+    int npts=10;
+    vector<Point3d> points3d;
+    vector<Point3d> points3d_estimated;
+    vector<Mat> projection_matrices;
+    vector<vector<Point2d> > points2d;
 
-  string filename(cvtest::TS::ptr()->get_data_path() + "sfm/rnd_N10_F3.yml");
-  readtestdata(filename, nviews, npts, points2d);
-  readtestdata(filename, nviews, projection_matrices);
-  readtestdata(filename, points3d);
+    string filename(cvtest::TS::ptr()->get_data_path() + "sfm/rnd_N10_F3.yml");
+    readtestdata(filename, nviews, npts, points2d);
+    readtestdata(filename, nviews, projection_matrices);
+    readtestdata(filename, points3d);
 
-  triangulatePoints(points2d,projection_matrices,
-      points3d_estimated);
+    triangulatePoints(points2d, projection_matrices, points3d_estimated);
 
-  cout << "Estimated 3D pts: " << points3d_estimated.size() << endl;
-  CV_Assert(points3d_estimated.size()==npts);
+    CV_Assert(points3d_estimated.size()==npts);
 
-  /*
-   cout << "Test data: " << filename << endl;
+    /*
+    cout << "Test data: " << filename << endl;
 
-   CV_Assert(points3d.size()==10);
+    CV_Assert(points3d.size()==10);
 
-   cout << "Ground truth 3D Points:" << endl;
-   for (int n = 0; n < points3d.size(); ++n)
-   cout << points3d[n] << endl;
-   */
+    cout << "Ground truth 3D Points:" << endl;
+    for (int n = 0; n < points3d.size(); ++n)
+    cout << points3d[n] << endl;
+    */
 
+}
+
+
+TEST(Sfm_triangulate, TriangulateDLT) {
+    libmv::TwoViewDataSet d = libmv::TwoRealisticCameras();
+
+    for (int i = 0; i < d.X.cols(); ++i)
+    {
+        libmv::Vec2 x1, x2;
+        libmv::MatrixColumn(d.x1, i, &x1);
+        libmv::MatrixColumn(d.x2, i, &x2);
+        libmv::Vec3 X_estimated, X_gt;
+        libmv::MatrixColumn(d.X, i, &X_gt);
+
+        // build x
+        vector<Mat> x;
+
+        Mat tmp;
+        eigen2cv<double,2,1>(x1, tmp);
+        x.push_back( tmp );
+
+        eigen2cv<double,2,1>(x2, tmp);
+        x.push_back( tmp );
+
+        // build P
+        vector<Mat> P;
+        eigen2cv<double,3,4>(d.P1, tmp);
+        P.push_back( tmp );
+
+        eigen2cv<double,3,4>(d.P2, tmp);
+        P.push_back( tmp );
+
+        Mat X_estimated_cv;
+        triangulatePoints(x, P, X_estimated_cv);
+        cv2eigen<double,3,1>(X_estimated_cv, X_estimated);
+
+//         std::cout << "X_estimated_cv: " << X_estimated_cv << std::endl;
+
+        EXPECT_NEAR(0, libmv::DistanceLInfinity(X_estimated, X_gt), 1e-8);
+    }
 }
