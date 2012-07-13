754bcb905556f23a5d2ebae80bda1e719a248a32
Srimal Jayawardena <srimalj@gmail.com>
2012-07-13 16:38:49 +1000

    Projective and non-projective data sets

--- src/libmv_opencv/testdata/cv/sfm/gendata.m
+++ src/libmv_opencv/testdata/cv/sfm/gendata.m
@@ -11,5 +11,8 @@
 % anim=generateToyAnimation( 0,'nPoint',5,'nFrame',3);
 % anim2cvyaml('rnd_N5_F3.yml',anim);
 
-anim=generateToyAnimation( 0,'nPoint',10,'nFrame',3);
-anim2cvyaml('rnd_N10_F3.yml',anim);
+anim=generateToyAnimation( 0,'nPoint',10,'nFrame',3,'isProj',1);
+anim2cvyaml('rnd_N10_F3_Proj1.yml',anim);
+
+anim=generateToyAnimation( 0,'nPoint',10,'nFrame',3,'isProj',0);
+anim2cvyaml('rnd_N10_F3_Proj0.yml',anim);
