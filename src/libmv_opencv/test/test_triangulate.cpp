6d3e4381f42c236984d68b4733fa1272062d34cd
Pablo Speciale <pablo.speciale@gmail.com>
2012-07-17 20:59:08 -0300

    Added cvtest::checkTriangulation(...) function. Generalization of the last nviews test.

--- src/libmv_opencv/test/test_triangulate.cpp
+++ src/libmv_opencv/test/test_triangulate.cpp
@@ -35,53 +35,66 @@
 
 #include "test_precomp.hpp"
 
-#include "libmv/multiview/test_data_sets.h"
-#include "libmv/numeric/numeric.h"
+namespace cvtest
+{
+  void
+  checkTriangulation(int nviews, int npoints, bool is_projective)
+  {
+    for(unsigned iter=0; iter<2; ++iter)
+    {
+      int depth;
+      float err_max2d, err_max3d;
+      if (iter==0)
+      {
+        depth = CV_32F;
+        err_max2d = 1e-5;
+        err_max3d = 1e-9;
+      }
+      else
+      {
+        depth = CV_64F;
+        err_max2d = 1e-7;
+        err_max3d = 1e-9;
+      }
 
-#include <opencv2/core/eigen.hpp>
+      cv::Mat K;
+      std::vector<cv::Mat> Rs;
+      std::vector<cv::Mat> ts;
+      std::vector<cv::Mat> Ps;
+      cv::Mat points3d;
+      std::vector<cv::Mat> points2d;
+      generateScene(nviews, npoints, is_projective, depth, K, Rs, ts, Ps, points3d, points2d);
 
-using namespace cv;
-using namespace std;
-using namespace cvtest;
+      // get 3d points
+      Mat X, X_homogeneous;
+      triangulatePoints(points2d, Ps, X);
+      EuclideanToHomogeneous(X, X_homogeneous);
+
+      for (int i = 0; i < npoints; ++i)
+      {
+        // Check reprojection error. Should be nearly zero.
+        for (int k = 0; k < nviews; ++k)
+        {
+          Mat x_reprojected;
+          HomogeneousToEuclidean( Ps[k]*X_homogeneous.col(i), x_reprojected );
+          double error = norm( x_reprojected - points2d[k].col(i) );
+          EXPECT_LE(error*error, err_max2d);
+        }
+        // Check 3d error. Should be nearly zero.
+        double error = norm( X.col(i) - points3d.col(i) );
+        EXPECT_LE(error*error, err_max3d);
+      }
+    }
+  }
+} /* namespace cvtest */
 
 
 TEST(Sfm_triangulate, TriangulateDLT) {
-    libmv::TwoViewDataSet d = libmv::TwoRealisticCameras();
+  int nviews = 2;
+  int npoints = 30;
+  bool is_projective = true;
 
-    Mat x1, x2;
-    Mat P1, P2;
-
-    eigen2cv<double, 2, Eigen::Dynamic>(d.x1, x1);
-    eigen2cv<double, 2, Eigen::Dynamic>(d.x2, x2);
-    eigen2cv<double,3,4>(d.P1, P1);
-    eigen2cv<double,3,4>(d.P2, P2);
-
-    // build x
-    vector<Mat> x;
-    x.push_back(x1);
-    x.push_back(x2);
-
-    // build P
-    vector<Mat> P;
-    P.push_back(P1);
-    P.push_back(P2);
-
-    // get 3d points
-    Mat X_estimated;
-    triangulatePoints(x, P, X_estimated);
-
-    // check
-    for (int i = 0; i < d.X.cols(); ++i)
-    {
-        libmv::Vec3 X_est, X_gt;
-
-        // get current columns
-        libmv::MatrixColumn(d.X, i, &X_gt);
-        cv2eigen<double,3,1>(X_estimated.col(i), X_est);
-
-        // Check: || X_est - X_gt ||_{inf} < 1e-8
-        EXPECT_NEAR(0, libmv::DistanceLInfinity(X_est, X_gt), 1e-8);
-    }
+  cvtest::checkTriangulation(nviews, npoints, is_projective);
 }
 
 TEST(Sfm_triangulate, NViewTriangulate_FiveViews)
@@ -90,49 +103,5 @@ TEST(Sfm_triangulate, NViewTriangulate_FiveViews)
   int npoints = 6;
   bool is_projective = true;
 
-  for(unsigned iter =0;iter<2;++iter)
-  {
-    int depth;
-    float err_max2d, err_max3d;
-    if (iter==0)
-    {
-      depth = CV_32F;
-      err_max2d = 1e-5;
-      err_max3d = 1e-9;
-    }
-    else
-    {
-      depth = CV_64F;
-      err_max2d = 1e-7;
-      err_max3d = 1e-9;
-    }
-
-    cv::Mat K;
-    std::vector<cv::Mat> Rs;
-    std::vector<cv::Mat> ts;
-    std::vector<cv::Mat> Ps;
-    cv::Mat points3d;
-    std::vector<cv::Mat> points2d;
-    generateScene(nviews, npoints, is_projective, depth, K, Rs, ts, Ps, points3d, points2d);
-
-    // get 3d points
-    Mat X, X_homogeneous;
-    triangulatePoints(points2d, Ps, X);
-    EuclideanToHomogeneous(X, X_homogeneous);
-
-    for (int i = 0; i < npoints; ++i)
-    {
-      // Check reprojection error. Should be nearly zero.
-      for (int k = 0; k < nviews; ++k)
-      {
-        Mat x_reprojected;
-        HomogeneousToEuclidean( Ps[k]*X_homogeneous.col(i), x_reprojected );
-        double error = norm( x_reprojected - points2d[k].col(i) );
-        EXPECT_LE(error*error, err_max2d);
-      }
-      // Check 3d error. Should be nearly zero.
-      double error = norm( X.col(i) - points3d.col(i) );
-      EXPECT_LE(error*error, err_max3d);
-    }
-  }
+  cvtest::checkTriangulation(nviews, npoints, is_projective);
 }
