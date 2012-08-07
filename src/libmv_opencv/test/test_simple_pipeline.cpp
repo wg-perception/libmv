14c45b2947d206aebf624acfd8430172610997ef
Pablo Speciale <pablo.speciale@gmail.com>
2012-08-07 05:49:27 -0300

    Added libmv_solveReconstruction() based on the 'libmv_capi' function (blender API). ToDo (pablo): check it is correct and move to "src/" folder

--- src/libmv_opencv/test/test_simple_pipeline.cpp
+++ src/libmv_opencv/test/test_simple_pipeline.cpp
@@ -35,12 +35,17 @@
 
 #include "test_precomp.hpp"
 
+#include "libmv/simple_pipeline/bundle.h"
+#include "libmv/simple_pipeline/camera_intrinsics.h"
+#include "libmv/simple_pipeline/initialize_reconstruction.h"
+#include "libmv/simple_pipeline/pipeline.h"
+#include "libmv/simple_pipeline/tracks.h"
+
 #include <fstream>
 #include <cstdlib>
 
 using namespace cv;
 using namespace std;
-
 
 /**
  * 2D tracked points (dinosaur dataset)
@@ -56,46 +61,133 @@ using namespace std;
  * through frames 1 to 36.  If there is no match found in a view then x
  * and y are -1.
  *
- * Each row corresponds to a different point.  There are 4983 values.
+ * Each row corresponds to a different point.
  *
+ * Link: http://www.robots.ox.ac.uk/~vgg/data/data-mview.html
  */
-void vgg_2D_tracked_points_parser( Mat &_values )
+void vgg_2D_tracked_points_parser( libmv::Tracks &libmv_tracks )
 {
-    // Copy 'viff.xy.txt' file (2.4 MB) into "testdata/cv/sfm/" folder.
-    // Dinosaur dataset: http://www.robots.ox.ac.uk/~vgg/data/data-mview.html
-    string filename = string(TEST_DATA_DIR) + "viff.xy.txt"; 
+    string filename = string(TEST_DATA_DIR) + "viff.xy.good_tracks.txt";
     ifstream file( filename.c_str() );
 
-    Mat_<double> values(4983, 36*2);
-    string token;
-    for (int row = 0; getline(file, token); ++row)
-    {
-        istringstream line(token);
+    const int height = 576;
 
-        int col = 0;
-        while (line >> token)
+    double x, y;
+    string str;
+
+    for (int track = 0; getline(file, str); ++track)
+    {
+        istringstream line(str);
+        bool is_first_time;
+
+        for (int frame = 0; line >> x >> y; ++frame)
         {
-            // cout << "row: " << row << " col: " << col << " Token :" << token << endl;
-            values(row, col) = atof(token.c_str());
-            col++;
+            // init track
+            if ( is_first_time && x > 0 && y > 0 )
+            {
+                y = height - y;                               // for blender: x = x/720;  y = (576-y)/576;
+                libmv_tracks.Insert( frame, track, x, y );
+                is_first_time = false;
+            }
+
+            // while tracking
+            else if ( x > 0 && y > 0 )
+            {
+                y = height - y;
+                libmv_tracks.Insert( frame, track, x, y );
+            }
+
+            // lost track
+            else if ( x < 0 && y < 0 )
+            {
+                is_first_time = true;
+            }
+
+            // some error
+            else
+            {
+                exit(1);
+            }
         }
-//         if (file.unget().get() == '\n')
-//         {
-//             // cout << "newline found" << endl;
-//         }
+    }
+}
+
+
+
+typedef struct libmv_Reconstruction
+{
+    libmv::EuclideanReconstruction reconstruction;
+
+    /* used for per-track average error calculation after reconstruction */
+    libmv::Tracks tracks;
+    libmv::CameraIntrinsics intrinsics;
+
+    double error;
+} libmv_Reconstruction;
+
+
+// ToDo (pablo): rewrite this, and move to "src/" folder
+// Based on the 'libmv_capi' function (blender API)
+void libmv_solveReconstruction(const libmv::Tracks &tracks, int keyframe1, int keyframe2,
+                               double focal_length, double principal_x, double principal_y, double k1, double k2, double k3,
+                               libmv_Reconstruction &libmv_reconstruction, bool refine_intrinsics = false)
+{
+    /* Invert the camera intrinsics. */
+    libmv::vector<libmv::Marker> markers = tracks.AllMarkers();
+    libmv::EuclideanReconstruction *reconstruction = &libmv_reconstruction.reconstruction;
+    libmv::CameraIntrinsics *intrinsics = &libmv_reconstruction.intrinsics;
+
+    intrinsics->SetFocalLength(focal_length, focal_length);
+    intrinsics->SetPrincipalPoint(principal_x, principal_y);
+    intrinsics->SetRadialDistortion(k1, k2, k3);
+
+    cout << "\tNumber of markers: " << markers.size() << endl;
+    for (int i = 0; i < markers.size(); ++i)
+    {
+        intrinsics->InvertIntrinsics(markers[i].x,
+                                     markers[i].y,
+                                     &(markers[i].x),
+                                     &(markers[i].y));
     }
 
-    values.copyTo( _values );
+    libmv::Tracks normalized_tracks(markers);
+
+    cout << "\tframes to init from: " << keyframe1 << " " << keyframe2 << endl;
+    libmv::vector<libmv::Marker> keyframe_markers =
+        normalized_tracks.MarkersForTracksInBothImages(keyframe1, keyframe2);
+    cout << "\tNumber of markers for init: " << keyframe_markers.size() << endl;
+
+    libmv::EuclideanReconstructTwoFrames(keyframe_markers, reconstruction);
+    libmv::EuclideanBundle(normalized_tracks, reconstruction);
+    libmv::EuclideanCompleteReconstruction(normalized_tracks, reconstruction);
+
+    // ToDo (pablo): autocalibration?
+//     if (refine_intrinsics) {
+//         libmv_solveRefineIntrinsics(tracks, intrinsics, reconstruction,
+//             refine_intrinsics, progress_update_callback, callback_customdata);
+//     }
+
+    libmv_reconstruction.tracks = tracks;
+    libmv_reconstruction.error = libmv::EuclideanReprojectionError(tracks, *reconstruction, *intrinsics);
 }
+
 
 
 TEST(Sfm_simple_pipeline, dinosaur)
 {
-    Mat_<double> values;
-    vgg_2D_tracked_points_parser( values );
+    libmv::Tracks tracks;
+    vgg_2D_tracked_points_parser( tracks );
 
-    EXPECT_DOUBLE_EQ( 197.44, values(4979,70));
-    EXPECT_DOUBLE_EQ( 260.19, values(4977,66));
+
+    int keyframe1 = 1, keyframe2 = 6;
+    double focal_length = 24, principal_x = 360, principal_y = 288, k1 = 0, k2 = 0, k3 = 0;
+    libmv_Reconstruction libmv_reconstruction;
+
+    libmv_solveReconstruction( tracks, keyframe1, keyframe2,
+                               focal_length, principal_x, principal_y, k1, k2, k3,
+                               libmv_reconstruction );
+
+    cout << "libmv_reconstruction.error = " << libmv_reconstruction.error << endl;
 
     // ToDo: complete the test
 //     FAIL();
