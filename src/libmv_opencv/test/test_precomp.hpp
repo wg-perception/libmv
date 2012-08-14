f000f19e435e36052b27d6bea2626b37362165ab
Pablo Speciale <pablo.speciale@gmail.com>
2012-08-14 03:38:16 -0300

    fix indentation

--- src/libmv_opencv/test/test_precomp.hpp
+++ src/libmv_opencv/test/test_precomp.hpp
@@ -39,25 +39,25 @@ do { \
 namespace cvtest
 {
 
-  /**
-  * 2D tracked points
-  * -----------------
-  *
-  * The format is:
-  *
-  * row1 : x1 y1 x2 y2 ... x36 y36 for track 1
-  * row2 : x1 y1 x2 y2 ... x36 y36 for track 2
-  * etc
-  *
-  * i.e. a row gives the 2D measured position of a point as it is tracked
-  * through frames 1 to 36.  If there is no match found in a view then x
-  * and y are -1.
-  *
-  * Each row corresponds to a different point.
-  *
-  */
-  void
-  parser_2D_tracks( const string &filename, libmv::Tracks &tracks );
+/**
+* 2D tracked points
+* -----------------
+*
+* The format is:
+*
+* row1 : x1 y1 x2 y2 ... x36 y36 for track 1
+* row2 : x1 y1 x2 y2 ... x36 y36 for track 2
+* etc
+*
+* i.e. a row gives the 2D measured position of a point as it is tracked
+* through frames 1 to 36.  If there is no match found in a view then x
+* and y are -1.
+*
+* Each row corresponds to a different point.
+*
+*/
+void
+parser_2D_tracks( const string &filename, libmv::Tracks &tracks );
 
 } // namespace cvtest
 
