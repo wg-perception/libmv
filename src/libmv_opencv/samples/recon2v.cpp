7bf4631f84ab4fbb45768824eca4d1a5eb0c9007
Srimal <srimalj@gmail.com>
2012-09-14 21:36:28 +1000

    Skeleton for simple sample.

--- src/libmv_opencv/samples/recon2v.cpp
+++ src/libmv_opencv/samples/recon2v.cpp
@@ -1,9 +1,44 @@
-#include<iostream>
+#include <opencv2/sfm/sfm.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+
 #include "recon2v.hpp"
 
 using namespace std;
+using namespace cv;
 
-int main()
+static void help()
 {
-  cout << "sample" << endl;
+    cout
+    << "\n------------------------------------------------------------------\n"
+    << " This program shows the two view reconstruction capabilities the \n"
+    << " OpenCV Structure From Motion (SFM) module.\n"
+    << " It uses the following data from the VGG datasets at ...\n"
+    << " Usage:\n"
+    << "       reconv2 view1_2D_pts.txt view2_2D_pts.txt\n"
+    << "------------------------------------------------------------------\n\n"
+    << endl;
+}
+
+
+int main( int, char** )
+{
+    help();
+    
+    // Do projective reconstruction
+    bool is_projective = true;
+    
+    // Assume noise free
+    bool has_outliers = false;
+    
+    // Read 2D points from text files
+    vector<Mat_<double> > points2d;
+//     
+//     Mat_<double> points3d_estimated;
+//     
+//     vector<Mat> Ps_estimated;
+//     
+//     reconstruct( points2d, Ps_estimated, points3d_estimated, is_projective, has_outliers );
+//     
+    return 0;
 }
