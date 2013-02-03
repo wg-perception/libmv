d7079586360d19d7f4bc4e2e88b5ac845226360f
Vincent Rabaud <vrabaud@willowgarage.com>
2013-02-03 13:56:12 +0100

    remove another function that's part of the latest OpenCV

--- src/libmv_opencv/include/opencv2/sfm/numeric.hpp
+++ src/libmv_opencv/include/opencv2/sfm/numeric.hpp
@@ -44,21 +44,6 @@
 
 namespace cv
 {
-
-  template<typename _Tp, int _rows, int _cols, int _options, int _maxRows, int _maxCols>
-  void eigen2cv( const Eigen::Matrix<_Tp, _rows, _cols, _options, _maxRows, _maxCols>& src,
-                 Matx<_Tp, _rows, _cols>& dst )
-  {
-      if( !(src.Flags & Eigen::RowMajorBit) )
-      {
-          dst = Matx<_Tp, _cols, _rows>(static_cast<const _Tp*>(src.data())).t();
-      }
-      else
-      {
-          dst = Matx<_Tp, _rows, _cols>(static_cast<const _Tp*>(src.data()));
-      }
-  }
-
 CV_EXPORTS
 void
 meanAndVarianceAlongRows( const Mat &A,
